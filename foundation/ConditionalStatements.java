156df5af95a77cf286a114963289b252deddc35c
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-01-02 22:12:50 +0530

    Create ConditionalStatements.java

---
+++ foundation/ConditionalStatements.java
@@ -0,0 +1,13 @@
+public class StatusCodeChecker {
+    public static void main(String[] args) {
+        // Variable declaration
+        int statusCode = 200;
+
+        // Conditional Logic
+        if (statusCode == 200) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+    }
+}
