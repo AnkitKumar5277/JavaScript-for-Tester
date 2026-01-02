4e483de22269785a4d3124abc717f61898e4fa81
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-01-02 22:16:07 +0530

    Update switch.java

--- foundation/switch.java
+++ foundation/switch.java
@@ -2,6 +2,11 @@ public class BrowserSelector {
     public static void main(String[] args) {
         String browser = "chrome";
 
+// Input Variable	.toLowerCase() ke baad	Match Hoga?
+// "CHROME"	"chrome"	✅ Yes
+// "Chrome"	"chrome"	✅ Yes
+// "chrome"	"chrome"	✅ Yes
+        
         switch (browser.toLowerCase()) {
             case "chrome":
                 System.out.println("Launching Google Chrome...");
