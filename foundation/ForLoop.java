9a0a4ca79f9fd1160bc2c36cef33d956552e47d4
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-01-02 22:25:56 +0530

    Create ForLoop.java

---
+++ foundation/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        
+        // for loop start: initialize; condition; increment
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+        
+    }
+}
